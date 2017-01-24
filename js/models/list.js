ce4845f0445eaabe5c4173050e1e1dbbe1d74b9c
FrankieMFerreira <FrankieMFerreira@gmail.com>
2017-01-24 15:30:10 -0500

    I changed

--- js/models/list.js
+++ js/models/list.js
@@ -1,3 +1,14 @@
 'use strict';
 // List Model
+var listIndex = -1
+var allLists = []
+function List(title){
+  this.title = title
+  this.id = listIndex ++
+  this.tasks = []
+  allLists.push(this)
 
+
+}
+
+List.all = allLists
--- js/models/task.js
+++ js/models/task.js
@@ -1,3 +1,11 @@
 'use strict';
 // Task Model
+function Task(description, priority, list){
+  this.description = description
+  this.priority = priority
+  this.id = list.tasks.length
+  list.tasks.push(this)
+  this.list = list
+  debugger
 
+}
